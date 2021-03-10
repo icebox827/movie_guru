72c6cb083d47c58c813ed184218cee2ad6dbe191
Denis Lafontant <icebox827@gmail.com>
2021-03-10 09:17:59 -0500

    Create migration for Category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_10_141522) do
+ActiveRecord::Schema.define(version: 2021_03_10_141734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,13 @@ ActiveRecord::Schema.define(version: 2021_03_10_141522) do
     t.integer "category_id"
     t.index ["author_id"], name: "index_articles_on_author_id"
     t.index ["category_id"], name: "index_articles_on_category_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.integer "priority"
   end
 
   create_table "users", force: :cascade do |t|
