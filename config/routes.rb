2295a4acee49da0f3fe789432c0646dac1039432
Denis Lafontant <icebox827@gmail.com>
2021-03-10 10:40:18 -0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   root 'sessions#new'
-  get 'users/new'
+ 
+  resources :users, only: %i[new create show]
   resources :sessions, only: %i[new create destroy]
-  resources :categories
-  resources :articles
+  resources :categories, only: %i[index new create show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :articles do
+    post '/vote/', to: 'votes#create'
+    delete '/vote/', to: 'votes#destroy'
+  end
   scope :sessions do
     get 'sign_out' => 'sessions#destroy'
   end
