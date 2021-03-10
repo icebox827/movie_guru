c5a9f166a0ec851ccb2caf053e80f03f609130b7
Denis Lafontant <icebox827@gmail.com>
2021-03-10 10:17:00 -0500

    Update routes and sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController
-  def new
+  def new; end
+
+  def create
+    user = User.find_by(name: params[:session][:name])
+    if user
+      # loggin
+      log_in(user)
+      redirect_to root_path, notice: 'You have logged in successfully!'
+    else
+      flash.now[:error] = 'Invalid User name!'
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
   get 'users/new'
+  resources :sessions, only: %i[new create destroy]
   resources :categories
   resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope :sessions do
+    get 'sign_out' => 'sessions#destroy'
+  end
 end
