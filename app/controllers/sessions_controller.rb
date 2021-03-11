df2de6fd3142cb49ee2d9e9d64ae9f54300c374e
Denis Lafontant <icebox827@gmail.com>
2021-03-11 15:38:06 -0500

    Update routes for sing in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     if @user
       session[:user_id] = @user.id
       session[:username] = @user.name
-      redirect_to user_path(@user)
+      redirect_to articles_path
     else
       flash.now[:danger] = 'Wrong login info'
       render 'new'
