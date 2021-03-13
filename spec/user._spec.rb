ac0701bd5eeeaf0a67c0d8611e4c7985ad029438
Denis Lafontant <icebox827@gmail.com>
2021-03-13 13:30:12 -0500

    Add test for user

---
+++ spec/user._spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe User do
+  name = 'Denis'
+
+  it 'create successfully' do
+    user = User.create(name: name)
+    expect(user).to be_valid
+  end
+
+  it 'create not successful' do
+    user = User.create
+    expect(user).to_not be_valid
+  end
+end
