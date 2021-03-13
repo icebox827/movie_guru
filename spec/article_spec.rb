ab343b5d7e5fa08066341f3dc028bbbf4a0ab8b1
Denis Lafontant <icebox827@gmail.com>
2021-03-13 13:56:49 -0500

    Add test for article using Rspec TSE changes

--- spec/article_spec.rb
+++ spec/article_spec.rb
@@ -1,16 +1,47 @@
 require 'rails_helper'
 
-RSpec.describe Category do
-  priority = 1
-  name = 'category'
+unless author = User.first
+  author = User.new(name: 'mohammed')
+  author.save
+end
+unless category = Category.first
+  category = Category.new(name: 'cat', priority: 1)
+  category.save
+end
 
-  it 'create successfully' do
-    category = Category.create(priority:priority, name: name)
-    expect(category).to be_valid
+title = 'Hello new world'
+short_title = 'as'
+text = 'This is testing text and it should be quite long to be valid'
+image = 'car.png'
+
+RSpec.describe Article, type: :model do
+  it 'is valid with valid attributes' do
+    article = author.articles.build(category_id: category.id, title: title, text: text, image: image)
+    expect(article).to be_valid
   end
 
-  it 'create not successful' do
-    category = Category.create
-    expect(category).to_not be_valid
+  it 'is not valid without a title' do
+    article = author.articles.build(category_id: category.id, text: text, image: image)
+    expect(article).to_not be_valid
+  end
+
+  it 'is not valid with a short title' do
+    article = author.articles.build(category_id: category.id, text: text, title: short_title, image: image)
+    expect(article).to_not be_valid
+  end
+
+  it 'is not valid without a text' do
+    article = author.articles.build(category_id: category.id, title: title, image: image)
+    expect(article).to_not be_valid
+  end
+
+  it 'is not valid without an author' do
+    article = Article.new(category_id: category.id, title: title, text: text, image: image)
+    expect(article).to_not be_valid
+  end
+
+  it 'is not valid without a category' do
+    article = Article.new(author_id: author.id, title: title, text: text, image: image)
+    expect(article).to_not be_valid
   end
 end
