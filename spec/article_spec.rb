f6de55c29580d835dd8b1481c2cc710c4136cd6c
Denis Lafontant <icebox827@gmail.com>
2021-03-13 13:42:10 -0500

    Remove comment code in session controller and article model TSE changes

---
+++ spec/article_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Category do
+  priority = 1
+  name = 'category'
+
+  it 'create successfully' do
+    category = Category.create(priority:priority, name: name)
+    expect(category).to be_valid
+  end
+
+  it 'create not successful' do
+    category = Category.create
+    expect(category).to_not be_valid
+  end
+end
