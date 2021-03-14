08c08184b12e27d36806522aa110af83651cf557
Denis Lafontant <icebox827@gmail.com>
2021-03-14 13:33:22 -0400

    Use assets pipeline for stylesheet

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module MovieGuru
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     # config/application.rb
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
